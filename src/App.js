8422d69dba37f7713130fecab74297cb67d5f762
Catalina Restrepo Uribe <catalina.restrepou@perficient.com>
2022-03-23 09:57:09 -0500

    [Add] Setup using create-react-app

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          DEV UNITED APP
         </a>
       </header>
     </div>
